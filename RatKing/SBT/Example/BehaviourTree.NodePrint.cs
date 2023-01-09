06119754cbd6e4321952adec02102890f875dbbe
Friedrich Hanisch <friedrich@ratking.de>
2023-01-09 21:52:25 +0100

    (Unity example) Make the Print node more versatile

--- RatKing/SBT/Example/BehaviourTree.NodePrint.cs
+++ RatKing/SBT/Example/BehaviourTree.NodePrint.cs
@@ -1,44 +1,85 @@
 #if UNITY_5_3_OR_NEWER
 using UnityEngine;
 
-// putting our node in RatKing.SBT makes things easier
+// putting our node(s) in RatKing.SBT makes things easier
 namespace RatKing.SBT {
 
 	public partial class BehaviourTree<T> {
 		/// <summary>
+		/// Print a dynamic debug message
+		/// </summary>
+		public BehaviourTree<T> Print(System.Func<string> message) // offering a method without debug name can make the tree more concise
+			=> Print((string)null, message);
+
+		/// <summary>
+		/// Print a dynamic debug message
+		/// </summary>
+		public BehaviourTree<T> Print(string name, System.Func<string> message) // offering a method with debug name can help with debugging via GenerateString()
+			=> Register(new NodePrintDynamic(this, name) { message = message });
+
+		/// <summary>
 		/// Print a fixed debug message
 		/// </summary>
 		public BehaviourTree<T> Print(string message) // offering a method without debug name can make the tree more concise
-			=> Print( null, message);
+			=> Print((string)null, message);
 
 		/// <summary>
 		/// Print a fixed debug message
 		/// </summary>
 		public BehaviourTree<T> Print(string name, string message) // offering a method with debug name can help with debugging via GenerateString()
-			=> Register(new NodePrint(this, name) { message = message });
+			=> Register(new NodePrintFixed(this, name) { message = message });
 
-		// the node
+		// the nodes
 
 		/// <summary>
-		/// PRINT a message
+		/// PRINT a dynamic message
 		/// </summary>
-		public class NodePrint : Node {
-			internal string message;
+		public class NodePrintDynamic : Node {
+			internal System.Func<string> message;
 
 			// always declare the standard constructor of a Node
-			public NodePrint(BehaviourTree<T> tree, string name)
+			public NodePrintDynamic(BehaviourTree<T> tree, string name)
 				: base(tree, name ?? "print") { }
 
 			// the clone method is needed if your Node has member variables
 			internal override Node Clone(BehaviourTree<T> otherTree, Node parent) {
-				var clone = (NodePrint)base.Clone(otherTree, parent);
+				var clone = (NodePrintDynamic)base.Clone(otherTree, parent);
 				// copy the member variables here
 				clone.message = message;
 				return clone;
 			}
 
 			//protected override void OnStart() {
-			// // override start for initialisation things, this is useful if the node's status is Status.Running
+			//	// override start for initialisation things, this is useful if the node's status is Status.Running
+			//}
+
+			// OnTick() is called every time the node 
+			protected override void OnTick() {
+				Debug.Log(message(), tree.target as Object);
+				curStatus = Status.Success; // set the status here
+			}
+		}
+
+		/// <summary>
+		/// PRINT a fixed message
+		/// </summary>
+		public class NodePrintFixed : Node {
+			internal string message;
+
+			// always declare the standard constructor of a Node
+			public NodePrintFixed(BehaviourTree<T> tree, string name)
+				: base(tree, name ?? "print") { }
+
+			// the clone method is needed if your Node has member variables
+			internal override Node Clone(BehaviourTree<T> otherTree, Node parent) {
+				var clone = (NodePrintFixed)base.Clone(otherTree, parent);
+				// copy the member variables here
+				clone.message = message;
+				return clone;
+			}
+
+			//protected override void OnStart() {
+			//	// override start for initialisation things, this is useful if the node's status is Status.Running
 			//}
 
 			// OnTick() is called every time the node 
